9348c82afb372db9c172ec35b29e33251f841545
Tomek <tjedrak@users.noreply.github.com>
2021-09-09 11:00:04 +0200

    Update and rename show-kes-period.sh to showKESperiod.sh

--- scripts/showKESperiod.sh
+++ scripts/showKESperiod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-NODE_HOME="/home/rebel"
 
 slotsPerKESPeriod=$(cat $NODE_HOME/config/mainnet-shelley-genesis.json | jq -r '.slotsPerKESPeriod')
 echo slotsPerKESPeriod: ${slotsPerKESPeriod}
-slotNo=$(cardano-cli query tip --mainnet | jq -r '.slotNo')
+slotNo=$(cardano-cli query tip --mainnet | jq -r '.slot')
 echo slotNo: ${slotNo}
 kesPeriod=$((${slotNo} / ${slotsPerKESPeriod}))
 echo kesPeriod: ${kesPeriod}
